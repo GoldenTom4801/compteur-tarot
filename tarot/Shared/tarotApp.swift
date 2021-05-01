2c889195f817fa506c3142be765ab4d149704dfb
GoldenTom4801 <thomas.loux@mail.pf>
2021-05-01 11:46:30 +0200

    fichier de base

---
+++ tarot/Shared/tarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  tarotApp.swift
+//  Shared
+//
+//  Created by Thomas Loux on 01/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct tarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
